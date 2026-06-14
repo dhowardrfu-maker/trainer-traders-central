a6a39b69fd8b75400688a9a6190a0f7203c5a7a1
David Howard <your@email.com>
2026-06-14 08:59:08 +0100

    Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { registerServiceWorker } from "./registerSW";
+import { inject } from "@vercel/analytics";
 
 createRoot(document.getElementById("root")!).render(<App />);
 
 registerServiceWorker();
+inject();
